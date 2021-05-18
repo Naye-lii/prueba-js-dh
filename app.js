85c0dbd813b897e8bac7459597a8d83bec44248e
Nayeli I. Hernández Zárate <nayitze@gmail.com>
2021-05-17 21:43:05 -0500

    se añadieron las lineas de las funciones

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const { constants } = require("node:buffer")
+
 /*
     FORMATO DE CADA TAREA
         - titulo: string
@@ -6,17 +8,22 @@
            * pendiente
          )
 */
-
+const fs = require ('fs') 
 exports.leerTareas = function () {
     /*
         En esta función debes leer y retornar las tareas registradas.
     */
+        const tareas = fs.readFileSync('tareas.json', 'utf-8')
+        return JSON.parse(tareas)
 }
 
 exports.agregarTarea = function (tarea) {
     /*
         Registra y guarda una nueva tarea.
     */
+        const tareas = fs.readFileSync('tareas.json', 'utf-8')
+        listaTareas = JSON.parse(tareas)
+        return listaTareas.push(tarea)
 }
 
 exports.filtrarTareasPorEstado = function (estado) {
@@ -24,4 +31,11 @@ exports.filtrarTareasPorEstado = function (estado) {
         En esta función debes de leer las tareas y retornar las que tengan el estado que se
         manda en el parametro.
     */
+        const tareas = fs.readFileSync('tareas.json', 'utf-8')
+        listaTareas = JSON.parse(tareas)
+        let s = [];
+        for (let i = 0; i < lisraTareas.length; i++ ){
+            listaTareas[i].estado == estado ? s = s.push(listaTareas[i]) : null ;
+        }
+        return s;
 }
