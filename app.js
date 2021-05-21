78f256c004c147cd3076884c624d794517b496ab
Nayeli I. Hernández Zárate <nayitze@gmail.com>
2021-05-20 22:45:47 -0500

    Verción funcional del examen, gracias por todo

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const { constants } = require("node:buffer")
 
 /*
     FORMATO DE CADA TAREA
@@ -13,17 +12,21 @@ exports.leerTareas = function () {
     /*
         En esta función debes leer y retornar las tareas registradas.
     */
-        const tareas = fs.readFileSync('tareas.json', 'utf-8')
-        return JSON.parse(tareas)
+        const tareas = fs.readFileSync('./tareas.json', 'utf-8')
+        let listado = JSON.parse(tareas);
+        return listado;
+
 }
 
 exports.agregarTarea = function (tarea) {
     /*
         Registra y guarda una nueva tarea.
     */
-        const tareas = fs.readFileSync('tareas.json', 'utf-8')
-        listaTareas = JSON.parse(tareas)
-        return listaTareas.push(tarea)
+        let listado = exports.leerTareas();
+        listado.push(tarea)
+        fs.writeFileSync('./tareas.json', JSON.stringify(listado));
+        listado = exports.leerTareas();
+        return listado;
 }
 
 exports.filtrarTareasPorEstado = function (estado) {
@@ -31,11 +34,12 @@ exports.filtrarTareasPorEstado = function (estado) {
         En esta función debes de leer las tareas y retornar las que tengan el estado que se
         manda en el parametro.
     */
-        const tareas = fs.readFileSync('tareas.json', 'utf-8')
-        listaTareas = JSON.parse(tareas)
-        let s = [];
-        for (let i = 0; i < lisraTareas.length; i++ ){
-            listaTareas[i].estado == estado ? s = s.push(listaTareas[i]) : null ;
+        let listado = exports.leerTareas();
+        let listaFiltrada =[];
+        for (let i = 0; i < listado.length; i++){
+            listado[i].status == estado ? listaFiltrada.push(listado[i]) : null;
         }
-        return s;
+        return listaFiltrada
+
+
 }
